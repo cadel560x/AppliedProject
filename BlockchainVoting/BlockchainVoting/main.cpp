8b8d6b958076e0b3ca70eba6dbc8a3ff10881945
Javier Mantilla <>
2019-01-08 17:16:30 +0000

    Setting up .gitignore

---
+++ BlockchainVoting/BlockchainVoting/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BlockchainVoting
+//
+//  Created by Javier Mantilla on 1/8/19.
+//  Copyright (c) 2019 GMIT. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
